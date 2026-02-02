a476fa1eccd7f85eebe6a5b2f2f33ec9ddee8956
Javier <javierraposohuelva@gmail.com>
2026-02-02 03:22:54 +0100

    first commit

---
+++ src/main/java/GestionVideoteca.java
@@ -0,0 +1,13 @@
+/**
+ * Esta clase ...
+ *
+ * @Autor: Javier Raposo Huelva
+ * @Version: 2026:01
+ */
+public class Main {
+    public static void main(String[] args) {
+        String uri = "mongodb+srv://admin:Usuario.25.@mongodb.o7p1hl1.mongodb.net/?appName=MongoDB";
+
+        
+    }
+}
